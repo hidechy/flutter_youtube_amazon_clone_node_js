7214e41fa2d38697e1357b385aff98dbe5e606e4
toyohide <hide.toyoda@gmail.com>
2023-10-03 15:26:38 +0900

    commit 20231003 login token get

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,34 +6,71 @@ const bcryptjs = require('bcryptjs');
 
 const authRouter = express.Router();
 
+const jwt = require('jsonwebtoken');
+
+
+
+/// signup
 authRouter.post('/api/signup', async (req, res) => {
 
-try {
-const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-const existingUser = await User.findOne({ email });
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res
+            .status(400)
+            .json({ msg: "User with same email already exists!" });
+        }
 
-if (existingUser) {
-return res
-.status(400)
-.json({ msg: "User with same email already exists!" });
-}
+        const hashedPassword = await bcryptjs.hash(password, 8);
 
-const hashedPassword = await bcryptjs.hash(password, 8);
+        let user = new User({
+            email,
+            password: hashedPassword,
+            name,
+        });
 
-let user = new User({
-email,
-password: hashedPassword,
-name,
+        user = await user.save();
+
+        res.json(user);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+
 });
 
-user = await user.save();
 
-res.json(user);
-} catch (e) {
-res.status(500).json({ error: e.message });
-}
+
+/// signin
+authRouter.post('/api/signin', async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user){
+            return res
+            .status(400)
+            .json({ msg: "User not exists!" });
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if (!isMatch){
+            return res
+            .status(400)
+            .json({ msg: "password wrong!" });
+        }
+
+        const token = jwt.sign({id: user._id}, "passwordKey");
+
+        res.json({token, ...user._doc});
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
 
 });
+
+
 
 module.exports = authRouter;
